f3c0fa026f33371b8f3b45aa1d5b5166c4375818
0xR3V <r3veal@protonmail.ch>
2023-10-06 17:08:49 +0300

    Adding the possibility to add clients to the database

---
+++ add_client_to_db.php
@@ -0,0 +1,37 @@
+<?php
+/* 2023-10-06 16:46:30 */
+require_once('connection/connectionConfig.php');
+
+$redirect_phrase = "Redirecting you to previous page in 3 seconds...";
+
+$Client_Username = $_GET["Client_Username"];
+
+$cuq = $dbh->prepare("SELECT Client_Username FROM Clients WHERE Client_Username = :username");
+$cuq->bindParam(':username', $Client_Username);
+$cuq->execute();
+
+if ($cuq->rowCount() > 0) {
+  echo "The User Exists... <br />";
+  echo $redirect_phrase;
+
+} else {
+  echo "The user doesn't exists... <br />";
+  echo "Adding user to database... <br />";
+
+  $stmt = $dbh->prepare("INSERT INTO Clients (Client_Username) VALUES (:username);");
+
+  $stmt->bindParam(':username', $Client_Username);
+  $stmt->execute();
+
+  echo "The user is added to the database... <br />";
+  echo $redirect_phrase;
+}
+
+
+echo '<script>
+        setTimeout(function() {
+          window.location.href = "' . $_SERVER['HTTP_REFERER'] . '";
+        }, 3000);
+      </script>';
+
+exit();
