a5b5762f875c772e54cef56a42ff8c21b7e22f05
0xR3V <r3veal@protonmail.ch>
2023-10-06 20:50:52 +0300

    Adding the possiblity to query tasks for each client from the db

---
+++ query_client_tasks.php
@@ -0,0 +1,42 @@
+<?php
+/* 2023-10-06 19:55:12 */
+require_once('connection/connectionConfig.php');
+
+$cuq = $dbh->prepare("SELECT Client_Username FROM Clients");
+$cuq->execute();
+
+if (!($cuq->rowCount() > 0)) {
+  echo "There are no usernames <br />";
+  echo "Redirecting you to add clients :)";
+
+  echo '<script>
+          setTimeout(function() {
+            window.location.href = "add_client.html";
+          }, 3000);
+        </script>';
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Query Client Tasks (2023-10-06 19:55:53)</title>
+  </head>
+  <body>
+    <h1>Query Client Tasks</h1>
+
+    <form action="query_client_tasks_from_db.php" method="GET">
+      <label for="queryClient">Client Username:</label>
+      <select id="queryClient" name="queryClient">
+        <?php
+        foreach ($cuq as $row) {
+          echo "<option value=\"$row[0]\">$row[0]</option>";
+        }
+        ?>
+      </select>
+      <br />
+
+      <button type="submit">Query Tasks</button>
+    </form>
+  </body>
+</html>
---
+++ query_client_tasks_from_db.php
@@ -0,0 +1,66 @@
+<?php
+/* 2023-10-06 20:15:44 */
+require_once('connection/connectionConfig.php');
+
+$redirect_phrase = "Redirecting you to previous page in 1 second...";
+
+$Client_Username = $_GET["queryClient"];
+
+$cuq = $dbh->prepare("SELECT * FROM Clients WHERE Client_Username = :username");
+$cuq->bindParam(':username', $Client_Username);
+$cuq->execute();
+
+if (!($cuq->rowCount() > 0)) {
+  echo "The User Doesn't Exists... <br />";
+  echo $redirect_phrase;
+
+  echo '<script>
+          setTimeout(function() {
+            window.location.href = "' . $_SERVER['HTTP_REFERER'] . '";
+          }, 1000);
+        </script>';
+} else {
+  $client = $cuq->fetch(PDO::FETCH_ASSOC);
+
+  $Client_ID = $client["Client_ID"];
+  $Client_Username = $client["Client_Username"];
+
+  $cuq = $dbh->prepare("SELECT Clients.Client_Username, Tasks.* FROM Clients, Tasks WHERE Tasks.Client_ID = :client_id AND Clients.Client_ID = :client_id; ");
+
+  $cuq->bindValue(':client_id', $Client_ID, PDO::PARAM_STR);
+  $cuq->execute();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title><?= $Client_Username ?>'s Tasks (2023-10-06 20:27:02)</title>
+
+    <link rel="stylesheet" href="css/table-style.css">
+  </head>
+  <body>
+    <h1><?= $Client_Username ?>'s Tasks</h1>
+
+    <table>
+      <thead>
+        <tr>
+          <th scope="col">Task Id</th>
+          <th scope="col">Task</th>
+          <th scope="col">Task Status</th>
+          <th scope="col">Task Location</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($cuq as $task) { ?>
+        <tr<?php if ($task["Task_Status"] == "Completed"): ?> class="completed"<?php elseif ($task["Task_Status"] == "In-Progress"): ?> class="inprogress"<?php else: ?> class="incomplete"<?php endif ?>>
+          <th scope="row"><?= $task["Task_ID"] ?></th>
+          <td><?= $task["Task"] ?></td>
+          <td><?= $task["Task_Location"] ?></td>
+          <td><?= $task["Task_Status"] ?></td>
+        </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </body>
+</html>
