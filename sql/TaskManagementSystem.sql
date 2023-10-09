23009dcae0523c1b32ddb4da63cd537197c3e4da
0xR3V <r3veal@protonmail.ch>
2023-10-09 19:46:06 +0300

    Changing Collation to utf8mb4_unicode_520_ci

--- sql/TaskManagementSystem.sql
+++ sql/TaskManagementSystem.sql
@@ -1,11 +1,15 @@
-CREATE DATABASE TaskManagementSystem;
+CREATE DATABASE TaskManagementSystem
+CHARACTER SET utf8mb4
+COLLATE utf8_unicode_520_ci;
 
 USE TaskManagementSystem;
 
 CREATE TABLE Clients (
     Client_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Client_Username VARCHAR(50) NOT NULL
-);
+)
+CHARACTER SET utf8mb4
+COLLATE utf8_unicode_520_ci;
 
 CREATE TABLE Tasks (
     Task_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -14,4 +18,6 @@ CREATE TABLE Tasks (
     Task TEXT NOT NULL,
     Task_Location VARCHAR(50) NOT NULL,
     FOREIGN KEY (Client_ID) REFERENCES Clients(Client_ID)
-);
+)
+CHARACTER SET utf8mb4
+COLLATE utf8_unicode_520_ci;
