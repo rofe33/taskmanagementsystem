6109526e44c2252ed6a9f9e3c4ca03386bfb857c
0xR3V <r3veal@protonmail.ch>
2023-10-09 19:49:48 +0300

    Removing default login credentials for XAMPP phpmyadmin.

--- connection/connectionConfig.php
+++ connection/connectionConfig.php
@@ -1,10 +1,10 @@
 <?php
 /* 2023-10-06 16:29:26 */
 
-$db_host = "localhost";
-$db_user = "root";
+$db_host = "";
+$db_user = "";
 $db_pass = "";
-$db_name = "TaskManagementSystem";
+$db_name = "";
 
 try {
   $dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
